604ea9fac3566d0d97a6689bd0d6100619620f55
manish14iitg <111654062+manish14iitg@users.noreply.github.com>
2025-05-28 23:55:20 +0530

    Update LoginButton.jsx

--- customer/src/components/LoginButton.jsx
+++ customer/src/components/LoginButton.jsx
@@ -13,7 +13,7 @@ function LoginButton({ onSuccess }) {
       const idToken = await result.user.getIdToken();
 
       const response = await axios.post(
-        "http://localhost:5000/api/auth/google",
+        "https://game-cart-backend.vercel.app/",
         {
           token: idToken,
         }
